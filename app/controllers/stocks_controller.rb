30f0047aaddb84d8f2c384b0da9391bb1cc88f41
LH <lh@lhdv.net>
2020-10-10 18:11:22 -0300

    Handle invalid stock ticker symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,19 @@
 class StocksController < ApplicationController
+
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    # render json: @stock
-    render 'users/portifolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        # render json: @stock
+        render 'users/portifolio'
+      else
+        flash[:alert] = "Please enter a valid stock ticker"
+        redirect_to portifolio_path  
+      end
+    else
+      flash[:alert] = "Please enter a valid stock ticker"
+      redirect_to portifolio_path
+    end
   end
+  
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,9 +8,13 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
     
-    # The code bellow is equal to the above it
-    # Stock.new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
-    new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
+    begin      
+      # The code bellow is equal to the above it
+      # Stock.new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
+      new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
+    rescue => exception
+      return nil
+    end
   end
 
 end
