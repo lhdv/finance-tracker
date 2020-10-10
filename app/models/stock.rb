071dc1ae0ae536a6a54fecdacb769d350c7b9042
LH <lh@lhdv.net>
2020-10-10 17:12:16 -0300

    Add Stock class method to retrieve iex API data

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+  
+  # Class method to lookup a stock price
+  def self.new_lookup(ticker)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_api[:publishable_token],
+      secret_token: Rails.application.credentials.iex_api[:secret_token],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.price(ticker)
+  end
+
 end
