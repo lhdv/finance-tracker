3333f74294d52dc166e7009eb7207bf0dd861fce
LH <lh@lhdv.net>
2020-10-13 21:41:14 -0300

    Add UserStock destroy action and add portifolio button to remove stock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,4 +10,13 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portifolio"
     redirect_to portifolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    if user_stock.destroy
+      flash[:notice] = "Stock #{stock.name} was successfully removed from your portifolio"
+      redirect_to portifolio_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   
   get 'search_stock', to: 'stocks#search'
   
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
 end
